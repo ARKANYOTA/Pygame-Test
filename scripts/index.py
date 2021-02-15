aa36dce5b951122a50df5f30b01417ad4e095860
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2021-02-15 14:01:16 +0100

    Perlin Noise class

--- scripts/index.py
+++ scripts/index.py
@@ -1,64 +1,68 @@
 import sys, pygame
 from vector2 import *
-from perlinnoise import perlin_noise, print_perlin_oise
+from perlinnoise import *
 from random import randint, uniform
 from math import sin, cos, floor
+from time import sleep
 
 def get_dir():
     return Vector2(1* pygame.key.get_pressed()[pygame.K_RIGHT] - 1* pygame.key.get_pressed()[pygame.K_LEFT],
                    1* pygame.key.get_pressed()[pygame.K_DOWN]  - 1* pygame.key.get_pressed()[pygame.K_UP])
-
 def randcol():
     return (randint(0,255), randint(0,255), randint(0,255))
-
 def clamp(val, a, b):
     return max(a, min(val, b))
 
-pygame.init()
-worldseed = uniform(-65536,65535)
-screensize = scrwidth, scrheight = 1100, 700
-screen = pygame.display.set_mode(screensize)
-DISPLAY = pygame.display.set_mode(screensize, 0, 32)
+def update_map(map, posy):
+    pass
+    # blkw=blockw
+    # del map[-1]
+    # map.insert(0,[0 for b in range(scrwidth // blockw)])
+    # for i in range(scrwidth // blockw) :
+    #     val = perlin_noise(i/10+0, 1/10+posy)
+    #     val = floor( (val+.5) *5)/5
+    #     val = clamp( (128*(val+1)), 0, 255)
+    #     if 190 < val:
+    #         map[0][i] = 255
+    #     elif 150 < val:
+    #         map[0][i] = 127
+    #     else:
+    #         map[0][i] = 0
+    # for h in range(len(map)) :
+    #     for w in range(len(map[0])) :
+    #         pygame.draw.rect(DISPLAY, (map[h][w], map[h][w], map[h][w]), (w*blkw, h*blkw, blkw, blkw))
 
-speed = [2, 2]
-noiseposy = 0
-blockw = 32
-scale = 10
-thresh = .5
-
-groundImg = pygame.image.load("dummyGround.png")
-groundImg = pygame.transform.scale(groundImg, (blockw, blockw))
 
 def main():
-    #WHITE = (255, 255, 255)
-    #BLUE = (0, 0, 255)
+    # Pygame & screen setup
+    pygame.init()
+    screensize = scrwidth, scrheight = 1100, 700
+    screen = pygame.display.set_mode(screensize)
+    DISPLAY = pygame.display.set_mode(screensize, 0, 32)
 
+    # Textures
+
+    # Game constants
+    BLOCKWIDTH = 32
+    SEED = uniform(-65536, 65535)
+
+    # Game variables
+    map = [[0 for k in range(scrwidth)] for i in range(scrheight)]
+    noise = PerlinNoise(SEED, 0, 0, 10)
+    noiseposy = 0
+    scale = 10
+
+    noise.print_perlin_noise(DISPLAY, 0, 0, scrwidth // BLOCKWIDTH, scrheight // BLOCKWIDTH, BLOCKWIDTH)
     while True:
         screen.fill((0,0,0))
         for event in pygame.event.get():
-            pass
             if event.type == pygame.QUIT:
                 sys.exit()
-        dirr = get_dir()
-        update = False
-        if dirr.x == 1:
-            scale += 0.05
-        elif dirr.x == -1:
-            scale -= 0.05
-        if dirr.y == -1:
-            noiseposy -= .1
-        elif dirr.y == 1:
-            noiseposy += .1
-        if pygame.key.get_pressed()[pygame.K_b]:
-            update = True
-            thresh -= 0.01
-            print(thresh)
-        if pygame.key.get_pressed()[pygame.K_n]:
-            update = True
-            thresh += 0.01
-            print(thresh)
-        print_perlin_noise(0, 0, 0, noiseposy, scrwidth // blockw, scrheight // blockw, scale, blockw)
-        pygame.display.flip()
 
+        noise.print_perlin_noise(DISPLAY, 0, 0, scrwidth//BLOCKWIDTH, scrheight//BLOCKWIDTH, BLOCKWIDTH)
+        noise.y -= 1
+
+        pygame.display.flip()
+        pygame.time.delay(500)
 
 main()
